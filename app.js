ce3a24a2778ecc8e2a2c5f52383342c235aefe5e
tonyOP1985 <toliverpaull@gmail.com>
2017-11-05 01:46:50 -0400

    Prepared app for heroku deployment

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
 	res.render('index');
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
